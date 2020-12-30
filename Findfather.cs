7ddf6e5ab3d79d5f5d21aa6b9c1a0551e5cb6ba5
peter008527979 <62128181+peter008527979@users.noreply.github.com>
2020-12-30 12:52:11 +0800

    Add files via upload

---
+++ Findfather.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Findfather : MonoBehaviour
+{
+    GameObject father_gameObject;   //宣告一個GameObject(用來放取得的子物件)。
+
+    void Start()
+    {   
+
+        father_gameObject = GameObject.Find("father");
+
+        // 宣告的物件 = father物件(利用Find尋找)。
+
+
+
+        gameObject.transform.parent = father_gameObject.transform;
+
+        // 自身物件的父母 = father物件。
+
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            gameObject.transform.parent = null;
+        } 
+    }
+}
