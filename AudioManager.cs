61a30a97d6075b9053e1b468b9b3ef8b54797d6c
peter008527979 <62128181+peter008527979@users.noreply.github.com>
2020-12-30 15:53:53 +0800

    Add files via upload

---
+++ AudioManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    //存放音频的数组
+    public AudioClip[] audios;
+    AudioSource Music;
+
+    public bool m_ToggleChange;
+
+    void Start()
+    {
+        
+        DontDestroyOnLoad(this.gameObject);
+        Music = GetComponent<AudioSource>();
+        Music.clip = audios[0];
+        Music.Play();
+
+    }
+    void Update()
+    {
+        //取場景名稱
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name == "Start")
+        {
+            m_ToggleChange = true;
+        }
+        if (scene.name == "TryThis")
+        {
+
+        }
+
+        if (scene.name == "Choice" && m_ToggleChange == true)
+        {
+            Music.clip = audios[1];
+            Music.Play();
+            m_ToggleChange = false;
+        }
+    }
+
+}
---
+++ MusicText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicText : MonoBehaviour
+{
+    public GameObject objPrefabInstantSource;//音乐预知物体 
+    private GameObject musicInstant = null;//场景中是否有这个物体  
+                                           // Use this for initialization  
+    void Start()
+    {
+
+        musicInstant = GameObject.FindGameObjectWithTag("Sound");
+        if (musicInstant == null)
+        {
+            musicInstant = (GameObject)Instantiate(objPrefabInstantSource);
+        }
+
+
+    }
+}
