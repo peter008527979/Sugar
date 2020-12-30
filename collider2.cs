9795ec12de9949fe2bbba87d57491b22b396c367
peter008527979 <62128181+peter008527979@users.noreply.github.com>
2020-12-30 15:37:28 +0800

    Add files via upload

---
+++ collider2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class collider2 : MonoBehaviour
+{
+    public Collider coll;
+
+    void Start()
+    {
+        coll = GetComponent<Collider>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            coll.enabled = true;
+        }
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        coll.enabled = false;
+        GameObject.Find("Background").GetComponent<cheak>().cheak2 += 1;
+    }
+
+
+}
---
+++ collider3.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class collider3 : MonoBehaviour
+{
+    public Collider coll;
+
+    void Start()
+    {
+        coll = GetComponent<Collider>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            coll.enabled = true;
+        }
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        coll.enabled = false;
+        GameObject.Find("Background").GetComponent<cheak>().cheak3 += 1;
+    }
+
+
+}
